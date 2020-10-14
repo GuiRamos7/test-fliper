a9506b1ed8889d2cdabbffc51cc3856862c95fd7
Guilherme Ramos <guilherme.ramos@matchboxbrasil.com>
2020-10-14 09:13:36 -0300

    Fixing font-family

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -6,7 +6,7 @@ export const Button = styled.button`
   padding: 7px 13px;
   border: 1px solid #3B5CB8;
   border-radius: 25px;
-  font-size: 14px;
+  font-size: 13px;
   background: #FFF;
   &:active {
     background: #f0f0f0;
